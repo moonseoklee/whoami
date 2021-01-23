767a238cb41e2c38a23cbdcd2a3b8e89156abc55
moon seok lee <winnyiee@korea.ac.kr>
2021-01-24 01:33:23 +0900

    iteration

--- src/components/detail/projects/projects.js
+++ src/components/detail/projects/projects.js
@@ -44,13 +44,13 @@ const Projects = () => {
     setIsOpen(false);
   }
 
-  const Card = () =>{
+  const Card = (project,i) =>{
     return(
       <div className="gallery-item" tabIndex="0" onClick={openModal}>
                 <img src="https://images.unsplash.com/photo-1511765224389-37f0e77cf0eb?w=500&h=500&fit=crop" className="gallery-image" alt="" />
                 <div className="gallery-item-info">
                   <ul>
-                    <li className="gallery-item-likes"><span className="visually-hidden">Likes:</span><i className="fas fa-heart" aria-hidden="true"></i> 56</li>
+                    <li className="gallery-item-likes"><span className="visually-hidden">Likes:</span><i className="fas fa-heart" aria-hidden="true"></i> {project.id}</li>
                     <li className="gallery-item-comments"><span className="visually-hidden">Comments:</span><i className="fas fa-comment" aria-hidden="true"></i> 2</li>
                   </ul>
                 </div>
@@ -59,12 +59,9 @@ const Projects = () => {
   }
   const CardList=projects.map(
     (project,i)=>(
-      Card()
+      Card(project,i)
     )
   )
-    
-  
-
 
   useEffect(() => {
     
@@ -112,19 +109,10 @@ const Projects = () => {
       </header>
       <main>
         <div className="container">
-          {CardList}
+          
           <div className="gallery">
-            
-            <div className="gallery-item" tabIndex="0" onClick={openModal}>
-              <img src="https://images.unsplash.com/photo-1511765224389-37f0e77cf0eb?w=500&h=500&fit=crop" className="gallery-image" alt="" />
-              <div className="gallery-item-info">
-                <ul>
-                  <li className="gallery-item-likes"><span className="visually-hidden">Likes:</span><i className="fas fa-heart" aria-hidden="true"></i> 56</li>
-                  <li className="gallery-item-comments"><span className="visually-hidden">Comments:</span><i className="fas fa-comment" aria-hidden="true"></i> 2</li>
-                </ul>
-              </div>
-            </div>
-            
+          {CardList}
+          
           
         <Modal
           isOpen={modalIsOpen}
