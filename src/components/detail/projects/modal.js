0763d08855de39c02ba98455af916309b7f8af5f
moon seok lee <winnyiee@korea.ac.kr>
2021-01-27 23:05:49 +0900

    iterate stacks

--- src/components/detail/projects/modal.js
+++ src/components/detail/projects/modal.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import ReactMarkdown from 'react-markdown'
 import styled from 'styled-components'
 
-const Stack = styled.img`
-  src={process.env.PUBLIC_URL+"/images/doraemon.png"}m
-  width:25px
+const StackDiv = styled.div`  
+  display:flex;
+`;
+
+const Stack = styled.img`  
+  height:25px;
+  margin-right:10px;
 `;
 
 const Contents = styled.div`   
@@ -15,17 +19,37 @@ const Thumbnail = styled.img`
   width:100%;
   src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"
 `;
+
+
 const ProjectsModal = (props) => {
-    return (
-        <>                  
-          <Thumbnail src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"></Thumbnail>
-          <Contents>
-            <Stack src={process.env.PUBLIC_URL+"/images/doraemon.png"}></Stack>
-          <div>Project {props.project.id}</div>
-          <ReactMarkdown># Hello, *world*!</ReactMarkdown>
-          </Contents>
-        </>
-    );
+  
+  
+  const StackList = () => {
+    const stacks = []
+    let sts = props.project.stack;
+    sts = sts.slice(2,-2)
+    sts = sts.split("','")
+   
+    
+    for(let i=0;i<sts.length;i++){
+      stacks.push(<Stack src={process.env.PUBLIC_URL + "/images/"+sts[i]+".png"}></Stack>);
+  }
+
+  return stacks;
+  }
+  
+  return (
+    <>
+      <Thumbnail src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"></Thumbnail>
+      <Contents>
+        <StackDiv>
+        <StackList></StackList>
+        </StackDiv>
+        <div>Project {props.project.id}</div>
+        <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+      </Contents>
+    </>
+  );
 };
 
 export default ProjectsModal;
