ac982aaa18706b872bcfb5cf4ee1c5ffefc3a206
moon seok lee <winnyiee@korea.ac.kr>
2021-02-05 00:09:52 +0900

    a

--- src/functions/api_refer.js
+++ src/functions/api_refer.js
@@ -4,8 +4,8 @@ export const GET_PROJECT = 'GET_PROJECT';
 export default async function getProjects(category) {
  
   return await axios
-    //.get('https://whoami-moonseok.herokuapp.com/item/'+category);
-    .get('http://127.0.0.1:8000/item/'+category);
+    .get('https://whoami-moonseok.herokuapp.com/item/'+category);
+    //.get('http://127.0.0.1:8000/item/'+category);
    
 }
 
