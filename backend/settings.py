5e708a4063016e671d27bfb6850d423772de8b15
moon seok lee <winnyiee@korea.ac.kr>
2021-02-08 21:57:13 +0900

    .

--- backend/settings.py
+++ backend/settings.py
@@ -162,7 +162,7 @@ DATABASES['default'].update(db_from_env)
 
 import os
 AWS_ACCESS_KEY_ID =os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ.['AWS_SECRET_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
